0db7ea7e1e0e8dba318ad153526750cc88d98d93
sandheepp <sandheep.p01@gmail.com>
2019-09-04 13:21:00 +0530

    changed master functinon

--- function.cpp
+++ function.cpp
@@ -5,5 +5,20 @@ for(int i=0;i<V;i++)
 
 void findloop(int dis[], src)
 {
-    
+    int smallest = __INT_MAX__;
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
+           if(dis[i][j]==0)
+           {
+               dis[i][j] = __INT_MAX__;
+           }
+            if ((dis[i][j]+dis[j][i])< smallest)
+            {
+                smallest =dis[i][j]+dis[j][i] ;
+            }
+        }        
+    }
+    print("%d",smallest);
 }
